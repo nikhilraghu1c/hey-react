50e42ec062c0623d49bf0e6e83c3ec2c58b3501b
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2023-07-28 23:24:34 +0530

    order and basics of react

--- App.js
+++ App.js
@@ -9,12 +9,19 @@
  * ReactElement(Object) => Converts Into HTML (Browser Understand) using render function
  * For Sibling you will pass 3rd parameter as array
  */
+
 const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm an H1 Tag"),
-        React.createElement("h2", {}, "I'm an H2 Tag")
+    [
+        React.createElement("div", { id: "child1" }, [
+            React.createElement("h1", {}, "I'm an H1 Tag"),
+            React.createElement("h2", {}, "I'm an H2 Tag")
+        ]),
+        React.createElement("div", { id: "child2" }, [
+            React.createElement("h3", {}, "I'm an H3 Tag"),
+            React.createElement("h4", {}, "I'm an H4 Tag")
+        ])
     ]
-    ));
+);
 
 // const heading = React.createElement("h1", { id: 'heading', style: { color: 'blue' } }, "Hello World From React!!");
 const root = ReactDOM.createRoot(document.getElementById('root'));
