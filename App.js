30cdb47444096fcaaef47fad8728ba2cc0451521
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2023-07-28 23:01:41 +0530

    Nested Structure & last sibling for react html

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Nested Structure & Sibling For React HTML
+ * <div id='parent'>
+ *      <div id='child'>
+ *          <h1>I'm an H1 Tag</h1>
+ *          <h2>I'm an H2 Tag </h2>
+ *      </div>
+ * </div>
+ * ReactElement(Object) => Converts Into HTML (Browser Understand) using render function
+ * For Sibling you will pass 3rd parameter as array
+ */
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "I'm an H1 Tag"),
+        React.createElement("h2", {}, "I'm an H2 Tag")
+    ]
+    ));
+
+// const heading = React.createElement("h1", { id: 'heading', style: { color: 'blue' } }, "Hello World From React!!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
