ee374244d8786e44b55a068c97a3ea9ea8acd18c
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2023-07-30 13:19:12 +0530

    Added React Functional Component

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement
-// JSX Javascript Syntax - is not html in js
-// It is HTML like or XML syntax
-// Creates React.createElement using babel
-
 const jsxHeading = (
   <h1 id="heading" className="head" tabIndex="1">
     Hey React Using JSX
   </h1>
 );
+
+// React Functional Component
+// Return JSX/ React Element
+// First letter of function should be Capital
+
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading">Hey React Functional Component</h1>
+    </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
-console.log(jsxHeading);
+root.render(<HeadingComponent />);
