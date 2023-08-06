dd623df3eb616857a0bc54081342c0ce16f2db5a
Nikhil Raghuwanshi <nraghuwanshi@planful.com>
2023-08-06 16:44:10 +0530

    Adding React Hook Use State

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,68 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  // Local State Variable  - Super Powerful Variable - React Hooks
+  const [listOfRestaurants, setListOfRestaurant] = useState([
+    {
+      info: {
+        id: "150602",
+        name: "Raimens Cafe 79",
+        cloudinaryImageId: "eohdm5sdnq6e2u46xeux",
+        costForTwo: "₹250 for two",
+        cuisines: [
+          "Fast Food",
+          "Pizzas",
+          "Chinese",
+          "Snacks",
+          "Beverages",
+          "Desserts",
+        ],
+        avgRating: 3.8,
+        sla: {
+          deliveryTime: 21,
+        },
+      },
+    },
+    {
+      info: {
+        id: "150604",
+        name: "Dominos",
+        cloudinaryImageId: "eohdm5sdnq6e2u46xeux",
+        costForTwo: "₹250 for two",
+        cuisines: [
+          "Fast Food",
+          "Pizzas",
+          "Chinese",
+          "Snacks",
+          "Beverages",
+          "Desserts",
+        ],
+        avgRating: 4.5,
+        sla: {
+          deliveryTime: 21,
+        },
+      },
+    },
+  ]);
   return (
     <div className="body">
-      <div className=" ">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top-Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           return (
             <RestaurantCard
               key={restaurant.info.id}
