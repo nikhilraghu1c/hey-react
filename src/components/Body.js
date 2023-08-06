f01c4b8ad23e15332cea0a10ebf5a99fec448903
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2023-08-06 17:26:08 +0530

    added res list

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,48 +4,7 @@ import resList from "../utils/mockData";
 
 const Body = () => {
   // Local State Variable  - Super Powerful Variable - React Hooks
-  const [listOfRestaurants, setListOfRestaurant] = useState([
-    {
-      info: {
-        id: "150602",
-        name: "Raimens Cafe 79",
-        cloudinaryImageId: "eohdm5sdnq6e2u46xeux",
-        costForTwo: "₹250 for two",
-        cuisines: [
-          "Fast Food",
-          "Pizzas",
-          "Chinese",
-          "Snacks",
-          "Beverages",
-          "Desserts",
-        ],
-        avgRating: 3.8,
-        sla: {
-          deliveryTime: 21,
-        },
-      },
-    },
-    {
-      info: {
-        id: "150604",
-        name: "Dominos",
-        cloudinaryImageId: "eohdm5sdnq6e2u46xeux",
-        costForTwo: "₹250 for two",
-        cuisines: [
-          "Fast Food",
-          "Pizzas",
-          "Chinese",
-          "Snacks",
-          "Beverages",
-          "Desserts",
-        ],
-        avgRating: 4.5,
-        sla: {
-          deliveryTime: 21,
-        },
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurant] = useState(resList); // Array Destructuring
   return (
     <div className="body">
       <div className="filter">
@@ -53,7 +12,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating >= 4
             );
             setListOfRestaurant(filteredList);
           }}
