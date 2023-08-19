a9265212f0a0f8eae0ff20ee85c12346add25368
Nikhil Raghuwanshi <nraghuwanshi@planful.com>
2023-08-19 18:36:34 +0530

    Added Route in the component

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <h2>This Is Contact Us Page</h2>
+        </div>
+    )
+}
+
+export default Contact;
