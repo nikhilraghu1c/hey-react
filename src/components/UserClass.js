25703005e8c03bee303c322781ea94b95a09cc62
Nikhil Raghuwanshi <nraghuwanshi@planful.com>
2023-08-26 12:11:45 +0530

    How to update the state variables

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,16 +5,23 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
-      count2: 1
     };
   }
   render() {
     const { name, location } = this.props;
-    const { count, count2 } = this.state;
+    const { count } = this.state;
     return (
       <div className="user-card">
         <h1>Count = {count}</h1>
-        <h1>Count2 = {count2}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+            });
+          }}
+        >
+          Count Increment
+        </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @nikhilraghu1c</h4>
